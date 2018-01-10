67cadc982f03d41e6c54ca8d2f993a222e3567b5
de6eling <de6eling@gmail.com>
2018-01-09 22:00:56 -0700

    first commit

---
+++ resources/assets/js/components/LeftMenu.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div id="left-menu">
+        <div class="content">
+            <h1>Progreso</h1>
+            <ul>
+                <li>
+                    <input type="checkbox" checked="checked"/>
+                    <input type="checkbox" checked="checked"/>
+                    <input type="checkbox" checked="checked"/>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+    #left-menu {
+        min-width: 240px;
+        max-width: 240px;
+        min-height: 100vh;
+        background-color: white;
+        box-shadow: 2px 1px 2px;
+    }
+    .content {
+        padding: 20px;
+    }
+
+    ul {
+        list-style: none;
+        padding-left: 0 !important;
+    }
+</style>
