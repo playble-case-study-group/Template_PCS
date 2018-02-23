26b8ba293db9bd05c2f2fc95c783c1c0ac92f855
abrax <abrax5000@gmail.com>
2018-02-23 11:31:42 -0700

    added seeder for content with markdown

--- database/seeds/WikiSeeder.php
+++ database/seeds/WikiSeeder.php
@@ -35,8 +35,12 @@ class WikiSeeder extends Seeder
         $en_id = DB::table('article')->insertGetId(
             [
                 'lang_id' => 1,
-                'title' => 'Test English',
-                'content' => '{"title": "Civil War", "content": "This is a civil war ", "subtitles": [{"title": "Civil War Subtitle 1", "content": "subtitle 1 contents", "subtitles": []}, {"title": "Civil Ware Subtitle 2", "content": "subtitle 2 contents", "subtitles": [{"title": "sub-sub-title", "content": "something", "subtitles": []}]}]}',
+                'title' => 'Civil War',
+                'content' => '# Civil War 
+this is some text
+## Madrid
+more text
+### Building',
 
             ]
         );
@@ -44,8 +48,12 @@ class WikiSeeder extends Seeder
         $sp_id = DB::table('article')->insertGetId(
             [
                 'lang_id' => 2,
-                'title' => 'Test Spanish',
-                'content' => '{"title": "Guerra Civil", "content": "hola taco burrito", "subtitles": [{"title": "Guerra Civil Subtitle 1", "content": "subtitle 1 contents", "subtitles": []}, {"title": "Guerra Civil Subtitle 2", "content": "subtitle 2 contents", "subtitles": [{"title": "sub-sub-title", "content": "something", "subtitles": []}]}]}',
+                'title' => 'Guerra Civil',
+                'content' => '# Guerra Civil
+Contenido uno
+## Madrid
+es una ciudad
+### Edificio',
 
             ]
         );
@@ -60,6 +68,40 @@ class WikiSeeder extends Seeder
 
         ]);
 
+        $en_id = DB::table('article')->insertGetId(
+            [
+                'lang_id' => 1,
+                'title' => 'Test English',
+                'content' => '# Test English
+Content uno
+## Test 2
+es una ciudad
+### Test 3',
 
+            ]
+        );
+
+        $sp_id = DB::table('article')->insertGetId(
+            [
+                'lang_id' => 2,
+                'title' => 'Test Spanish',
+                'content' => '# Test Spanish
+Contenido uno
+## Test test
+es una ciudad
+### blah',
+
+            ]
+        );
+
+
+
+        DB::table('wiki')->insert([
+            [
+                'lang_1_ar' => $en_id,
+                'lang_2_ar' => $sp_id
+            ]
+
+        ]);
     }
 }
