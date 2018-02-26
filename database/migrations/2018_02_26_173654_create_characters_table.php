a9629a75e2343f44e4d16a16f8e1819ed86f6c5a
Matalyn T <matalynt@Dereks-iMac.local>
2018-02-26 10:58:49 -0700

    Added database migrations

---
+++ database/migrations/2018_02_26_173654_create_characters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->char('name');
+            $table->char('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2018_02_26_175241_create_video_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('char_id');
+            $table->integer('day');
+            $table->string('url');
+            $table->string('question');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamp('updated_at');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video');
+    }
+}
