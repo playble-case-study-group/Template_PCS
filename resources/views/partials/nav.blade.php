f466f6be90f8a2fa22bbe0897c25484072d1e2b8
Daniel Ebeling <danielebeling@Daniels-iMac.app.byu.edu>
2018-06-12 14:21:33 -0600

    odds and ends

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -54,7 +54,9 @@
                     </a>
 
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="/classes">Classes</a>
+                        @if(Auth::user()->role == 2)
+                            <a class="dropdown-item" href="/classes">Classes</a>
+                        @endif
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                      document.getElementById('logout-form').submit();">
